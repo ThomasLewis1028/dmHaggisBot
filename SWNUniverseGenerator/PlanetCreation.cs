6659c6492b9354a7d0b35b15859540f607340636
Thomas Lewis <thomasalewis1028@gmail.com>
2020-03-20 16:07:23 -0500

    Fixed AddPlanets not adding planets to each star I had it set up where it would only create up to pMax planets total. Wrapped it in a foreach and now it will create up to pMax for each Star.
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

--- SWNUniverseGenerator/PlanetCreation.cs
+++ SWNUniverseGenerator/PlanetCreation.cs
@@ -15,42 +15,44 @@ namespace SWNUniverseGenerator
             var starData = LoadStarData();
             
             var planLen = starData.Planets.Count;
+            
+            foreach(Star star in universe.Stars){
+                var pMax = planetDefaultSettings.PlanetRange == null || planetDefaultSettings.PlanetRange.Length == 0 ||
+                           planetDefaultSettings.PlanetRange[0] == 0 ||
+                           planetDefaultSettings.PlanetRange[1] == 0
+                    ? 4
+                    : rand.Next(planetDefaultSettings.PlanetRange[0],
+                        planetDefaultSettings.PlanetRange[1] + 1);
 
-            var pMax = planetDefaultSettings.PlanetRange == null || planetDefaultSettings.PlanetRange.Length == 0 ||
-                       planetDefaultSettings.PlanetRange[0] == 0 ||
-                       planetDefaultSettings.PlanetRange[1] == 0
-                ? 4
-                : rand.Next(planetDefaultSettings.PlanetRange[0], 
-                    planetDefaultSettings.PlanetRange[1] + 1);
-            
-            var pCount = 0;
-            
-            while (pCount < pMax)
-            {
-                var planet =
-                    new Planet(starData.Planets[rand.Next(0, planLen)]);
-                    
-                if (universe.Planets.Exists(a => a.Name == planet.Name))
-                    continue;
-                    
-                
-                IDGen.GenerateID(planet);
-                
-                planet.StarID = universe.Stars[rand.Next(0, universe.Stars.Count)].ID;
-                planet.WorldTag = worldInfo.WorldTags[rand.Next(0, 100)];
-                planet.Atmosphere = worldInfo.Atmospheres[rand.Next(0, 6) + rand.Next(0, 6)];
-                planet.Temperature = worldInfo.Temperatures[rand.Next(0, 6) + rand.Next(0, 6)];
-                planet.Biosphere = worldInfo.Biospheres[rand.Next(0, 6) + rand.Next(0, 6)];
-                planet.Population = worldInfo.Populations[rand.Next(0, 6) + rand.Next(0, 6)];
-                planet.TechLevel = worldInfo.TechLevels[rand.Next(0, 6) + rand.Next(0, 6)];
-                planet.Origin = worldInfo.OWOrigins[rand.Next(0, 8)];
-                planet.Relationship = worldInfo.OWRelationships[rand.Next(0, 8)];
-                planet.Contact = worldInfo.OWContacts[rand.Next(0, 8)];
-                
-                universe.Planets.Add(planet);
-                pCount++;
+                var pCount = 0;
+
+                while (pCount < pMax)
+                {
+                    var planet =
+                        new Planet(starData.Planets[rand.Next(0, planLen)]);
+
+                    if (universe.Planets.Exists(a => a.Name == planet.Name))
+                        continue;
+
+
+                    IDGen.GenerateID(planet);
+
+                    planet.StarID = star.ID;
+                    planet.WorldTag = worldInfo.WorldTags[rand.Next(0, 100)];
+                    planet.Atmosphere = worldInfo.Atmospheres[rand.Next(0, 6) + rand.Next(0, 6)];
+                    planet.Temperature = worldInfo.Temperatures[rand.Next(0, 6) + rand.Next(0, 6)];
+                    planet.Biosphere = worldInfo.Biospheres[rand.Next(0, 6) + rand.Next(0, 6)];
+                    planet.Population = worldInfo.Populations[rand.Next(0, 6) + rand.Next(0, 6)];
+                    planet.TechLevel = worldInfo.TechLevels[rand.Next(0, 6) + rand.Next(0, 6)];
+                    planet.Origin = worldInfo.OWOrigins[rand.Next(0, 8)];
+                    planet.Relationship = worldInfo.OWRelationships[rand.Next(0, 8)];
+                    planet.Contact = worldInfo.OWContacts[rand.Next(0, 8)];
+
+                    universe.Planets.Add(planet);
+                    pCount++;
+                }
             }
-            
+
             return universe;
         }
         

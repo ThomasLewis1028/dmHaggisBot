b021e9420735ea7a362b3737e88b27d1528bdf56
Thomas Lewis <thomasalewis1028@gmail.com>
2020-03-21 15:57:02 -0500

    Added comments to Grid.cs
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

--- SWNUniverseGenerator/Grid.cs
+++ SWNUniverseGenerator/Grid.cs
@@ -1,15 +1,31 @@
-﻿namespace SWNUniverseGenerator
+﻿using System;
+
+namespace SWNUniverseGenerator
 {
+    /// <summary>
+    /// Grid object that stores an X and Y length
+    /// </summary>
     public class Grid
     {
+        /// <summary>
+        /// Constructor requires an X and Y value to create the grid
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public Grid(int x, int y)
         {
             X = x;
             Y = y;
         }
 
-        public int X { get; set; }
+        /// <summary>
+        /// The X length of a grid
+        /// </summary>
+        public Int32 X { get; set; }
 
-        public int Y { get; set; }
+        /// <summary>
+        /// The Y length of a grid
+        /// </summary>
+        public Int32 Y { get; set; }
     }
 }

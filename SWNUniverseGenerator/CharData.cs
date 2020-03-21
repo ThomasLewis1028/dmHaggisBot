f4b87d45cfbf005c2293a8c8cd90c4e8431cba0d
Thomas Lewis <thomasalewis1028@gmail.com>
2020-03-21 14:31:35 -0500

    Added comments to CharData.cs
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

--- SWNUniverseGenerator/CharData.cs
+++ SWNUniverseGenerator/CharData.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 
 namespace SWNUniverseGenerator
 {
+    /// <summary>
+    /// This is where a characterData.json file is deserialized to
+    /// </summary>
     internal class CharData
     {
+        /// <summary>
+        /// Holds the String List of Male First names
+        /// </summary>
         public List<String> MaleName { get; set; }
+        
+        /// <summary>
+        /// Holds the String List of Female First names 
+        /// </summary>
         public List<String> FemaleName { get; set; }
+        
+        /// <summary>
+        /// Holds the String List of all Last Names
+        /// </summary>
         public List<String> LastName { get; set; }
+        
+        /// <summary>
+        /// Holds the String List of HairColors
+        /// </summary>
         public List<String> HairColor { get; set; }
+        
+        /// <summary>
+        /// Holds the String List of HairStyles
+        /// </summary>
         public List<String> HairStyle { get; set; }
+        
+        /// <summary>
+        /// Holds the String List of EyeColors
+        /// </summary>
         public List<String> EyeColor { get; set; }
     }
 }

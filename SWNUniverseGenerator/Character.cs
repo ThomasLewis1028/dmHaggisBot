caf867094f5efebae06fbe0a564d1c0cdd97acdd
Thomas Lewis <thomasalewis1028@gmail.com>
2020-03-21 13:33:30 -0500

    Added comments and summaries to Character.cs
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

--- SWNUniverseGenerator/Character.cs
+++ SWNUniverseGenerator/Character.cs
@@ -1,47 +1,104 @@
 ﻿using System;
-using Json.Net;
 using Newtonsoft.Json;
 
 namespace SWNUniverseGenerator
 {
+    /// <summary>
+    /// Character object that stores 
+    /// </summary>
     public class Character : IEntity
     {
+        /// <summary>
+        /// An Enum for a list of Genders. Undefined should never be used.
+        /// </summary>
         public enum GenderEnum
         {
             Male,
             Female,
             Undefined
         }
+        
         //private Ship ship;
 
-        public Character(string first, string last)
+        /// <summary>
+        /// All Characters should have a first and last name associated with them.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        public Character(String first, String last)
         {
             First = first;
             Last = last;
         }
 
+        /// <summary>
+        /// The unique String ID of a Character. This should be randomly generated to assure uniqueness.
+        /// </summary>
         public String ID { get; set; }
-        public string First { get; set; }
-        public string Last { get; set; }
-        public int Age { get; set; }
+        
+        /// <summary>
+        /// First name of the Character
+        /// </summary>
+        public String First { get; set; }
+        
+        /// <summary>
+        /// Last name of the Character
+        /// </summary>
+        public String Last { get; set; }
+        
+        /// <summary>
+        /// Integer Age of the Character 
+        /// </summary>
+        public Int32 Age { get; set; }
+        
+        /// <summary>
+        /// A unique String ID of a Planet to mark the Character's birth planet
+        /// </summary>
         public String BirthPlanet { get; set; }
+        
+        /// <summary>
+        /// A unique String ID of a Planet to mark the Character's current Location.
+        /// Location can be a planet, a city, or other various location types. 
+        /// </summary>
         public String CurrentLocation { get; set; }
-        public string HairStyle { get; set; }
-        public string HairCol { get; set; }
-        public string EyeCol { get; set; }
-        public string Title { get; set; }
+        
+        /// <summary>
+        /// A string value of a Character's HairStyle
+        /// </summary>
+        public String HairStyle { get; set; }
+        
+        /// <summary>
+        /// A string value of a Character's HairColor
+        /// </summary>
+        public String HairCol { get; set; }
+        
+        /// <summary>
+        /// A string value of a Character's EyeColor
+        /// </summary>
+        public String EyeCol { get; set; }
+        
+        /// <summary>
+        /// A string value of a Character's Title
+        /// </summary>
+        public String Title { get; set; }
+        
+        /// <summary>
+        /// A Enum for the Character's Gender.
+        /// </summary>
         public GenderEnum Gender { get; set; }
 
+        /// <summary>
+        /// This will return the string values stored in Gender as opposed to an index integer
+        /// </summary>
+        /// <returns>String value of the Gender</returns>
         public String GetGender()
         {
             return Gender.ToString();
         }
 
+        /// <summary>
+        /// Returns the concatenation of the First and Last name without storing it in the Json
+        /// </summary>
         [JsonIgnore] public string Name => First + " " + Last;
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
     }
 }

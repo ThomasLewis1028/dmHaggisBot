48a12c3137e5c75870acf8b94da7b9c15d6e53f5
Thomas Lewis <thomasalewis1028@gmail.com>
2020-03-21 16:53:20 -0500

    Added comments to IDGen.cs and changed ifs to a switch I saw a little thing on Rider that said "convert to switch" and so I used that to convert it to a switch, and then it said "convert to switch" again and it set it to a switch where it sets the ID based on the switch. So thanks JetBrains for being smarter than I am.
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

--- SWNUniverseGenerator/IDGen.cs
+++ SWNUniverseGenerator/IDGen.cs
@@ -1,28 +1,36 @@
 ﻿using System;
-using System.Linq;
 
 namespace SWNUniverseGenerator
 {
+    /// <summary>
+    /// This class and method handles the creation of randomly generated ID for each IEntity in the Generator
+    /// </summary>
     public class IDGen
     {
+        /// <summary>
+        /// This method receives an generic IEntity item and then creates an ID based on its type and assigns it to
+        /// the Object.ID
+        /// </summary>
+        /// <param name="item"></param>
+        /// <typeparam name="T"></typeparam>
         public static void GenerateID<T>(T item) where T : IEntity
         {
-            var id = Guid.NewGuid().ToString().Substring(0, 8);
-            
-            if (item is Planet)
-                id = "P-" + id;
-            else if (item is Star)
-                id = "S-" + id;
-            else if (item is Character)
-                id = "U-" + id;
-            else if (item is City)
-                id = "C-" + id;
-            else if (item is Problem)
-                id = "Q-" + id;
-            else
-                id = "Z-" + id;
+            // Create a Guid and chop everything from the first hyphen and on
+            var id = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
 
-            item.ID = id.ToUpper();
+            // Set the ID based on the type of Object
+            id = item switch
+            {
+                Planet _ => ("P-" + id),
+                Star _ => ("S-" + id),
+                Character _ => ("U-" + id),
+                City _ => ("C-" + id),
+                Problem _ => ("Q-" + id),
+                _ => ("Z-" + id)
+            };
+
+            // Set the ID
+            item.ID = id;
         }
     }
 }

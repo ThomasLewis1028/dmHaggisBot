dc6976335cb917937f71fbbe91c51b543e330ea4
Thomas Lewis <thomasalewis1028@gmail.com>
2020-04-28 22:02:27 -0500

    Added Alien and Beast data
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

---
+++ SWNUniverseGenerator/DeserializedObjects/AlienData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SWNUniverseGenerator.DeserializedObjects
+{
+    public class AlienData
+    {
+        public List<String> BodyTraits { get; set; }
+        
+        public List<Lense> Lenses { get; set; }
+        
+        public List<SocialStructure> SocialStructures { get; set; }
+    }
+
+    public class Lense
+    {
+        public String Type { get; set; }
+        
+        public String Description { get; set; }
+    }
+    
+    public class SocialStructure
+    {
+        public String Type { get; set; }
+        
+        public String Description { get; set; }
+    }
+}

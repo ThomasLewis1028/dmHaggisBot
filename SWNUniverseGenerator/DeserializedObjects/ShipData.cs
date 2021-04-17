b419fc75b4a00e46965bfa64ea2fe540387f5d54
Thomas Lewis <thomasalewis1028@gmail.com>
2021-04-16 20:25:35 -0600

    Reformatting, started adding presets. This is good enough to close out the ticket.
    Signed-off-by: Thomas Lewis <thomasalewis1028@gmail.com>

--- SWNUniverseGenerator/DeserializedObjects/ShipData.cs
+++ SWNUniverseGenerator/DeserializedObjects/ShipData.cs
@@ -12,9 +12,9 @@ namespace SWNUniverseGenerator.DeserializedObjects
         public List<Defense> Defenses { get; set; }
 
         public List<Weapon> Weapons { get; set; }
-        
+
         public List<Presets> Presets { get; set; }
-        
+
         public Naming Naming { get; set; }
     }
 
@@ -98,17 +98,17 @@ namespace SWNUniverseGenerator.DeserializedObjects
         public Int32 Cost { get; set; }
 
         public Int32? AmmoCost { get; set; }
-        
+
         public String Dmg { get; set; }
 
         public Int32 Power { get; set; }
 
         public Int32 Mass { get; set; }
-        
+
         public Int32 Hardpoints { get; set; }
 
         public String Class { get; set; }
-        
+
         public Int32 TechLevel { get; set; }
 
         public String Qualities { get; set; }
@@ -119,31 +119,33 @@ namespace SWNUniverseGenerator.DeserializedObjects
     public class Presets
     {
         public String HullType { get; set; }
-        
+
         public List<Preset> ListPresets { get; set; }
     }
 
     public class Preset
     {
         public String PresetName { get; set; }
-        
+
         public Int32 CrewSkill { get; set; }
-        
+
         public Int32 Cp { get; set; }
-        
+
         public List<Int32> Weapons { get; set; }
-        
+
         public List<Int32> Defenses { get; set; }
-        
+
         public List<Int32> Fittings { get; set; }
     }
 
     public class Naming
     {
         public List<String> Adjectives { get; set; }
-        
+
         public List<String> Animals { get; set; }
 
         public List<String> Nouns { get; set; }
+
+        public List<String> Presets { get; set; }
     }
 }

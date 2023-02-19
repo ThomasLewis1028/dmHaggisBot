07398ecbe0da86278ab2eebe9eaf260e5df555b4
Thomas Lewis <thomasalewis1028@gmail.com>
2023-02-19 14:57:38 -0700

    Added the files I forgot to add

---
+++ SWNBlazorApp/Data/CharacterService.cs
@@ -0,0 +1,22 @@
+﻿using SWNUniverseGenerator;
+using SWNUniverseGenerator.Models;
+
+namespace SWNBlazorApp.Data;
+
+public class CharacterService
+{
+    public Task<List<Character>> GetCharactersAsync(Universe universe)
+    {
+        return Task.FromResult(universe.Characters);
+    }
+
+    public Task<bool> CreateCharacterAsync()
+    {
+        CreationService creationService = new CreationService();
+        Creation creation = creationService.GetCreationAsync().Result;
+
+        // UniverseService
+        
+        return Task.FromResult(true);
+    } 
+}

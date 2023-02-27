125d0e4205cf89823a44a599c8f019576bed6942
Michael Lewis <factotuminok@gmail.com>
2023-02-26 23:23:50 -0600

    Continue creating data seed

--- SWNUniverseGenerator/Migrations/20230227052156_InitialCreate.Designer.cs
+++ SWNUniverseGenerator/Migrations/20230227052156_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SWNUniverseGenerator.Database;
 
@@ -10,9 +11,11 @@ using SWNUniverseGenerator.Database;
 namespace SWNUniverseGenerator.Migrations
 {
     [DbContext(typeof(UniverseContext))]
-    partial class UniverseContextModelSnapshot : ModelSnapshot
+    [Migration("20230227052156_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.3");
--- SWNUniverseGenerator/Models/Job.cs
+++ SWNUniverseGenerator/Models/Job.cs
@@ -3,9 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SWNUniverseGenerator.Models
 {
-    public class Job
+    public class Job: BaseEntity
     {
-        public String JobId { get; set; }
         public Item Cargo { get; set; }
         public Character Contact { get; set; }
         public Planet Dest { get; set; }

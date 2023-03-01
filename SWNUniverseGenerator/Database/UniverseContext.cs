bd0ad7dabcca5f782adb0222b9e72adfd09c6660
Thomas Lewis <thomasalewis1028@gmail.com>
2023-02-28 19:55:34 -0700

    Added ShipSpec.json to get presets

--- SWNUniverseGenerator/Database/UniverseContext.cs
+++ SWNUniverseGenerator/Database/UniverseContext.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using Microsoft.EntityFrameworkCore;
-using SWNUniverseGenerator.DeserializedObjects;
 using SWNUniverseGenerator.Migrations;
 using SWNUniverseGenerator.Models;
 

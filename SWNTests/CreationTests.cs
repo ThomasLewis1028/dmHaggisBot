2b4351dc8be9da32d5ea5600c67706a8f7522b6c
Thomas Lewis <thomasalewis1028@gmail.com>
2023-03-03 23:02:23 -0700

    Changed CreationFullTest to use DeleteRange

--- SWNTests/CreationTests.cs
+++ SWNTests/CreationTests.cs
@@ -36,28 +36,23 @@ public class CreationTests
             if (cleanup)
             {
                 using (var starRepo = new Repository<Star>(context))
-                    foreach (var star in context.Stars.Where(s => s.UniverseId == universeId))
-                        starRepo.Delete(star);
+                    starRepo.DeleteRange(context.Stars.Where(c => c.UniverseId == universeId).ToList());
 
                 using (var planRepo = new Repository<Planet>(context))
-                    foreach (var planet in context.Planets.Where(s => s.UniverseId == universeId))
-                        planRepo.Delete(planet);
+                    planRepo.DeleteRange(context.Planets.Where(c => c.UniverseId == universeId).ToList());
 
                 using (var zoneRepo = new Repository<Zone>(context))
-                    foreach (var zone in context.Zones.Where(z => z.UniverseId == universeId))
-                        zoneRepo.Delete(zone);
+                    zoneRepo.DeleteRange(context.Zones.Where(c => c.UniverseId == universeId).ToList());
 
                 using (var uniRepo = new Repository<Universe>(context))
                     uniRepo.Delete(universeId);
-                
+
                 using (var charRepo = new Repository<Character>(context))
-                    foreach (var character in context.Characters.Where(c => c.UniverseId == universeId))
-                        charRepo.Delete(character);
+                    charRepo.DeleteRange(context.Characters.Where(c => c.UniverseId == universeId).ToList());
                 
                 using (var shipRepo = new Repository<Ship>(context))
-                    foreach (var ship in context.Ships.Where(c => c.UniverseId == universeId))
-                        shipRepo.Delete(ship);
-                
+                    shipRepo.DeleteRange(context.Ships.Where(c => c.UniverseId == universeId).ToList());
+
                 Assert.IsTrue(context.Universes.Count(u => u.Id == universeId ) == 0);
                 Assert.IsTrue(context.Zones.Count(s => s.UniverseId == universeId) == 0);
                 Assert.IsTrue(context.Stars.Count(s => s.UniverseId == universeId) == 0);
